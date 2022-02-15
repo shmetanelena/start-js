17094e485b786ada36bc76a4cadf1ccaa5ace3ff
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-02-15 21:57:15 +0200

    add task 17 modul 2

--- js/modul2.js
+++ js/modul2.js
@@ -1,48 +1,3 @@
-<<<<<<< Updated upstream
-console.log('Module 1.2');
-
-// ====== isNaN vs Number.isNaN ======
-// === isNaN ===
-// console.log(isNaN('a')); // true
-// console.log(isNaN('b')); // true
-// console.log(isNaN('bsdkfjgh')); // true
-// console.log(isNaN(undefined)); // true
-// console.log(isNaN(NaN)); // true
-
-// console.log(isNaN(false)); // 0 - false
-// console.log(isNaN(true)); // 1 - false
-// console.log(isNaN('123')); // 123 - false
-// console.log(isNaN(123)); // 123 - false
-
-// // === Number.isNaN ===
-// console.log(Number.isNaN(NaN));
-
-// console.log(Number.isNaN('a')); // false
-// console.log(Number.isNaN('b')); // false
-// console.log(Number.isNaN('bsdkfjgh')); // false
-// console.log(Number.isNaN(undefined)); // false
-// console.log(Number.isNaN(false)); // 0 - false
-// console.log(Number.isNaN(true)); // 1 - false
-// console.log(Number.isNaN('123')); // 123 - false
-// console.log(Number.isNaN(123)); // 123 - false
-
-// ============ break ========== используется для нахождения
-// числа в массиве и оатановки после нахождения
-
-// for (let i = 1; i <= 10; i += 1) {
-//   if (i >= 6) {
-//     break;
-//   }
-//   console.log(i);
-// }
-// ================
-// ============= continue =====
-// for (let i = 1; i <= 10; i += 1) {
-//   if (i % 2 === 0) {
-//     // выводит только нечетные
-//     continue;
-//   }
-=======
 // console.log('Module 2.1');
 // const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
 // console.table(friends); // удобно выводит массив
@@ -133,12 +88,12 @@ console.log('Module 1.2');
 
 // colors.splice(1, 0, 'yellow', 'pink');
 // console.log(colors); // ["red", "yellow", "pink", "green", "blue"]
+
 // //========== task 17 modul 2 =========
 // const start = 3;
 // const end = 7;
 
 // for (let i = start; i <= end; i += 1) {
 //   // Change this line
->>>>>>> Stashed changes
 //   console.log(i);
 // }
