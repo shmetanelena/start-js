2973ab546b642f7f454ca07ae9f81612aad68978
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-02-16 00:00:40 +0200

    add task 20 modul 2

--- js/modul2.js
+++ js/modul2.js
@@ -97,3 +97,25 @@
 //   // Change this line
 //   console.log(i);
 // }
+// ======== task 20 modul 2 ============
+// Напиши функцию calculateTotalPrice(order),
+//     которая принимает один параметр order -
+//     массив чисел, и рассчитывает общую сумму его элементов.
+//     Общая сумма элементов должна сохраняться в переменной total,
+//     которая возвращается, как результат работы функции.
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+  for (const number of order) {
+    total += number;
+  }
+  //   for (let i = 0; i < order.length; i += 1) {
+  //     total += order[i];
+  //     //Change code above this line
+  return total;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// console.log(calculateTotalPrice());
