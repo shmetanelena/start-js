e1aa9792e6e026597a523d793beef40adea7fd8c
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-02-15 21:51:31 +0200

    add task 17 modul 2

--- js/modul1.js
+++ js/modul1.js
@@ -426,3 +426,301 @@
 
 // console.log(`Общая сумма потраченого в магазине: ${totalSpent}`);
 // =========================
+<<<<<<< Updated upstream
+=======
+// Модуль 1. Занятие 2. Ветвления. Циклы
+
+// Example 1 - Ввод пользователя и ветвления
+
+// Используя конструкцию if..else и prompt, напиши код,
+//   который будет спрашивать: "Какое официальное название JavaScript?".
+//   Если пользователь вводит ECMAScript, то показывай alert со
+//   строкой "Верно!", в противном случае - "Не знаете? ECMAScript!"
+// ===== решение с помощью if ========
+// const userInput = prompt('Какое официальное название JavaScript?');
+// let answer;
+// if (userInput !== null && userInput.toLocaleLowerCase() === 'ecmascript') {
+//   answer = 'Верно';
+// } else {
+//   answer = 'Не знаете? ECMAScript!';
+// }
+
+// console.log(answer);
+
+// ===== решение с помощью тернарника ========
+// const userInput = prompt('Какое официальное название JavaScript?');
+// let answer =
+//   userInput?.toLocaleLowerCase() === 'ecmascript'
+//     ? 'Верно'
+//     : 'Не знаете? ECMAScript!';
+
+// console.log(answer);
+
+// Example 2 - Отображение времени (if...else)
+
+// Напиши скрипт для отображения часов и минут
+// в консоли браузера в виде строки формата "14 ч. 26 мин.".
+// Если значение переменной minutes равно 0,
+// то выводи строку "14 ч.", без минут.
+// ==== с помощью тернарника =====
+
+// const hours = 14;
+// const minutes = 0;
+// let timestring = minutes > 0 ? `${hours} ч. ${minutes} мин.` : `${hours} ч.`;
+// console.log(timestring);
+
+// ==== с помощью if else =====
+// const hours = 14;
+// const minutes = 0;
+// let timestring;
+// if (minutes > 0) {
+//   timestring = `${hours} ч. ${minutes} мин.`;
+// } else {
+//   timestring = `${hours} ч.`;
+// }
+// console.log(timestring);
+
+// Example 3 - Ветвеления
+
+// Напиши скрипт, который выводит в консоль строку
+// "Это положительное число", если в prompt пользователь ввел число
+// больше нуля.
+// Если был введен ноль, выводи в консоль строку "Это ноль".
+// Если передали отрицательное число, в консоли должна
+// быть строка "Это отрицательное число".
+
+// const userInput = prompt('Введите число');
+// const userInputNumer = Number(userInput);
+
+// if (Number.isNaN(userInputNumer) || !userInput) {
+//   // !userInput аналогично записи userInput === 0
+//   console.log('Это не число');
+// } else if (userInput > 0) {
+//   console.log('Это положительное число');
+// } else if (userInput < 0) {
+//   console.log('Это отрицательное число');
+// } else {
+//   console.log('Это нуль');
+// }
+
+// Example 4 - Вложенные ветвления
+
+// Напиши скрипт, который сравнивает числа в переменных a и b.
+// Если оба значения больше 100, то выведи в консоль максимальное из них.
+// В противном случае в консоли должна быть сумма значения b и числа 512.
+
+// const a = 1200;
+// const b = 180;
+
+// if (a > 100 && b > 100) {
+//   //   if (a > b) {
+//   //     console.log(`Максимальное значение: ${a}`);
+//   //   } else {
+//   //     console.log(`Максимальное значение: ${b}`);
+//   //   }
+//   console.log(`Максимальное значение: ${a > b ? a : b}`);
+// } else {
+//   console.log(b + 512);
+// }
+
+// Example 5 - Форматирование ссылки (endsWith)
+
+// Напиши скрипт который проверяет заканчивается ли значение
+// переменной link символом /.
+// Если нет, добавь в конец значения link этот символ.
+// Используй конструкцию if...else.
+
+// let link = 'https://my-site.com/about/';
+// // Пиши код ниже этой строки
+// if (!link.endsWith('/')) {
+// if (link[link.length - 1] !== '/') {  // второй вариант решения
+//   link += '/';
+// }
+// // // Пиши код выше этой строки
+// console.log(link);
+
+// Example 6 - Форматирование ссылки (includes и логическое «И»)
+
+// Напиши скрипт который проверяет заканчивается ли значение переменной link символом /. Если нет, добавь в конец значения link этот символ, но только в том случае, если в link есть подстрока "my-site". Используй конструкцию if...else.
+
+// let link = 'https://somesite.com/about';
+// // Пиши код ниже этой строки
+
+// // Пиши код выше этой строки
+// console.log(link);
+
+// Example 7 - Форматирование ссылки (тернарный оператор)
+
+// Выполни рефакторинг кода задачи номер 4 используя
+// тернарный оператор.
+
+// let link = 'https://my-site.com/about';
+// // if (link.includes('my-site') && !link.endsWith('/')) {
+// //   link += '/';
+// // }
+// link += link.includes('my-site') && !link.endsWith('/') ? '/' : '';
+// console.log(link);
+
+// Example 8 - if...else и логические операторы
+
+// Напиши скрипт который будет выводить в консоль браузера
+// строку в зависимости от значения переменной hours.
+
+// Если значение переменной hours:
+
+// меньше 17, выводи строку "Pending"
+// больше либо равно 17 и меньше либо равно 24,
+// выводи строку "Expires"
+// больше 24 , выводи строку "Overdue"
+
+// const hours = 25;
+// if (hours <= 17) {
+//   console.log('Pending');
+// } else if (hours >= 17 && hours <= 24) {
+//   console.log('Expires');
+// } else {
+//   console.log('Overdue');
+// }
+// Example 9 - Дедлайн сдачи проекта (if...else)
+
+// Напиши скрипт для отображения времени дедлайна сдачи проекта.
+// Используй конструкцию if...else.
+
+// Eсли до дедлайна 0 дней - выведи строку "Сегодня"
+// Eсли до дедлайна 1 день - выведи строку "Завтра"
+// Eсли до дедлайна 2 дня - выведи строку "Послезавтра"
+// Eсли до дедлайна 3+ дней - выведи строку "Дата в будущем"
+// const daysUntilDeadline = 0;
+// // Пиши код ниже этой строки
+
+// if (daysUntilDeadline === 0) {
+//   console.log('Ночью спать не придется');
+// } else if (daysUntilDeadline === 1) {
+//   console.log('Завтра');
+// } else if (daysUntilDeadline === 2) {
+//   console.log('Послезавтра');
+// } else {
+//   console.log('Дата в будущем');
+// }
+
+// Example 10 - Дедлайн сдачи проекта (switch)
+
+// Выполни рефакторинг кода задачи номер 5 используя switch.
+
+// const daysUntilDeadline = 5;
+
+// switch (daysUntilDeadline) {
+//   case 0:
+//     console.log('Сегодня спать не придется');
+//     break;
+//   case 1:
+//     console.log('Завтра');
+//     break;
+//   case 2:
+//     console.log('Послезавтра');
+//     break;
+//   default:
+//     console.log('Дата в будущем');
+//     break;
+// }
+
+// Example 11 - Цикл for
+
+// Напиши цикл for который выводит в консоль браузера
+// числа по возрастанию от min до max,
+// но только если число кратное 5.
+
+// const max = 100;
+// const min = 20;
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+// Example 12 - Ввод пользователя и ветвления
+
+// Напиши скрипт, который будет спрашивать логин с помощью prompt
+// и логировать результат в консоль браузера.
+
+// Если посетитель вводит "Админ", то prompt запрашивает пароль
+// Если ничего не введено или нажата клавиша Esc -
+// вывести строку "Отменено"
+// В противном случае вывести строку "Я вас не знаю"
+// Пароль проверять так:
+
+// Если введён пароль "Я админ", то вывести строку "Здравствуйте!"
+// Иначе выводить строку "Неверный пароль"
+// const login = prompt('enter login');
+
+// if (login === 'Админ') {
+//   const password = prompt('enter password');
+//   if (password === 'Я админ') {
+//     console.log('Здравствуйте!');
+//   } else {
+//     console.log('Неверный пароль');
+//   }
+// } else {
+//   if (login === null |`| login === '') {
+//     console.log('Отменено');
+//   } else {
+//     console.log('Я вас не знаю');
+//   }
+// }
+
+// ======== !!!!! G A M E !!! =====
+
+// =========== вопросы на занятии =========
+// === slice пример применени в бегущей строке ==========
+// ===== у меня не получилось запустить !!! =============
+// const text =
+//   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, laboriosam nostrum saepe odio fugiat rerum facilis consequuntur corporis, sit aut eius sapiente atque nisi ut cumque? Dolore cumque hic reiciendis.';
+
+// const title = document.querySelector('h3');
+
+// for (let i = 0; i < text.length; i += 1) {
+//   const str = text.slice(i);
+//   console.log(str);
+//   setTimeout(() => {
+//     title.textContent = str;
+//   }, i * 10);
+// }
+// =========== это код Гери из его ГитХаба, он тоже не запустился=====
+// const text =
+//   'Метод slice() возвращает новый массив, содержащий копию части исходного массива ';
+
+// const title = document.querySelector('h3');
+
+// for (let i = 0; i < text.length; i += 1) {
+//   const str = text.slice(i);
+
+//   setTimeout(() => {
+//     title.textContent = str;
+//   }, i * 100);
+// }
+// ==============
+// ====== isNaN vs Number.isNaN ======
+// === isNaN ===
+// console.log(isNaN('a')); // true
+// console.log(isNaN('b')); // true
+// console.log(isNaN('bsdkfjgh')); // true
+// console.log(isNaN(undefined)); // true
+// console.log(isNaN(NaN)); // true
+
+// console.log(isNaN(false)); // 0 - false
+// console.log(isNaN(true)); // 1 - false
+// console.log(isNaN('123')); // 123 - false
+// console.log(isNaN(123)); // 123 - false
+
+// // === Number.isNaN ===
+// console.log(Number.isNaN(NaN));
+
+// console.log(Number.isNaN('a')); // false
+// console.log(Number.isNaN('b')); // false
+// console.log(Number.isNaN('bsdkfjgh')); // false
+// console.log(Number.isNaN(undefined)); // false
+// console.log(Number.isNaN(false)); // 0 - false
+// console.log(Number.isNaN(true)); // 1 - false
+// console.log(Number.isNaN('123')); // 123 - false
+// console.log(Number.isNaN(123)); // 123 - false
+// //  проверка
+>>>>>>> Stashed changes
--- js/modul2.js
+++ js/modul2.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 console.log('Module 1.2');
 
 // ====== isNaN vs Number.isNaN ======
@@ -41,5 +42,103 @@ console.log('Module 1.2');
 //     // выводит только нечетные
 //     continue;
 //   }
+=======
+// console.log('Module 2.1');
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.table(friends); // удобно выводит массив
+// console.log(friends[2]); // обратились ко второму элементу
+// friends[1] = 'qweqwe'; //изменение элемента массива (запись сложного типа)
+// friends[3] = 123123;
+// console.table(friends);
+// const lastIndex = friends.length - 1; // номер последнего элемента
+// console.log('Последний элемент: ', lastIndex);
+// // =======мктод split =====  split(delimeter) превращает строку в массив,
+// // «разбив» её по разделителю
+// const name = 'Mango';
+// console.log(name.split('')); // ["M", "a", "n", "g", "o"]
+
+// const message = 'JavaScript это интересно';
+// console.log(message.split(' ')); // ["JavaScript", "это", "интересно"]
+// //======join(delimeter) соединяет элементы массива в строку. ===
+// const words = ['JavaScript', 'это', 'интересно'];
+// console.log(words.join('')); // "JavaScriptэтоинтересно"
+// console.log(words.join(' ')); // "JavaScript это интересно"
+// console.log(words.join('-')); // "JavaScript-это-интересно"
+
+// // ========== indexOf(value) возвращает первый индекс,
+// //в котором элемент со значением value============
+// const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// console.log(clients.indexOf('Poly')); // 2
+// console.log(clients.indexOf('Monkong')); // -1
+
+// //=========includes(value) проверяет есть ли в массиве элемент
+// //со значением value ==========
+// const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// console.log(clients.includes('Poly')); // true
+// console.log(clients.includes('Monkong')); // false
+
+// //===========Проверка множественных условий с includes()​==========
+// // Выносим варианты в массив
+// const redFruits = ['apple', 'strawberry', 'cherry', 'cranberries'];
+// const fruit = 'cherry';
+// // Проверяем наличие элемента
+// const hasFruit = redFruits.includes(fruit);
+
+// if (hasFruit) {
+//   console.log(`${fruit} is a red fruit!`);
+// }
+// =======slice(begin, end) возвращает новый массив,
+// содержащий копию части исходного массива, не изменяя его.
+// Копия делается от begin и до, но не включая, end =======
+// const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// console.log(clients.slice(1, 3)); // ["Ajax", "Poly"]
+
+//===========Метод splice() если исходный массив нужно изменить.
+// // Удаляет, добавляет и заменяет элементы в произвольном месте массива.
+// // Чтобы удалить элементы в массиве, передаются два аргумента.
+
+// // splice(position, num)
+// // Скопировать
+// // position - указывает позицию (индекс) первого элемента для удаления
+// // num - определяет количество удаляемых элементов
+
+// const scores = [1, 2, 3, 4, 5];
+
+// // Удаляем три элемента массива, начиная с первого элемента (индекс 0)
+// const deletedScores = scores.splice(0, 3);
+
+// // Теперь массив scores содержит два элемента
+// console.log(scores); // [4, 5]
+
+// // А массив deletedScores содержит три удаленных элемента
+// console.log(deletedScores); // [1, 2, 3]
+
+// // Для того чтобы добавить один или несколько элементов в массив, необходимо передать три или более аргумента, при этом второй аргумент должен быть равен нулю.
+
+// // splice(position, 0, new_element_1, new_element_2, ...)
+
+// // Аргумент position указывает начальную позицию в массиве, куда будут вставлены новые элементы.
+// // Второй аргумент это ноль, он говорит методу не удалять элементы в месте добавления новых.
+// // Третий, четвертый и все последующие аргументы - это новые элементы, которые добавляются в массив.
+//Добавим новый цвет перед элементом с индексом 2.
+
+// const colors = ['red', 'green', 'blue'];
+
+// colors.splice(2, 0, 'purple');
+// console.log(colors); // ["red", "green", "purple", "blue"]
+
+// добавить произвольное количество элементов, передав четвертый, пятый аргумент и т. д.
+
+// const colors = ['red', 'green', 'blue'];
+
+// colors.splice(1, 0, 'yellow', 'pink');
+// console.log(colors); // ["red", "yellow", "pink", "green", "blue"]
+// //========== task 17 modul 2 =========
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) {
+//   // Change this line
+>>>>>>> Stashed changes
 //   console.log(i);
 // }
