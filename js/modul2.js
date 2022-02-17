d4898496eb0006b24cfb7732a83b6c987708e89a
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-02-17 10:12:59 +0200

    add 23 task

--- js/modul2.js
+++ js/modul2.js
@@ -206,13 +206,32 @@ const loginToFind = 'poly1scute';
  * при условии что числа уникальные (не повторяются).
  */
 
-const numbers = [51, 18, 13, 24, 7, 85, 19];
-let smallestNumber = numbers[0];
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let smallestNumber = numbers[0];
 
-for (const number of numbers) {
-  if (number < smallestNumber) {
-    smallestNumber = number;
-  }
-}
+// for (const number of numbers) {
+//   if (number < smallestNumber) {
+//     smallestNumber = number;
+//   }
+// }
 
-console.log('smallestNumber: ', smallestNumber);
+// console.log('smallestNumber: ', smallestNumber);
+//========== task 23 modul 2 =====
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const newArray = [];
+//   for (const number of numbers) {
+//     if (number > value);
+//     {
+//       newArray.push(number);
+//     }
+//   }
+
+//   return newArray;
+//   // Change code above this line
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
