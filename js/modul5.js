437e6ca421aff15fb04f9acc0188d0d116058559
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-03-16 19:24:07 +0200

    task2

---
+++ js/modul5.js
@@ -0,0 +1,111 @@
+// === task 1
+{
+    const pizzaPalace = {
+        pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+        // Change code below this line
+        checkPizza(pizzaName) {
+            return this.pizzas.includes(pizzaName);
+        },
+        order(pizzaName) {
+            const isPizzaAvailable = this.checkPizza(pizzaName);
+
+            if (!isPizzaAvailable) {
+                return `Sorry, there is no pizza named «${pizzaName}»`;
+            }
+
+            return `Order accepted, preparing «${pizzaName}» pizza`;
+        },
+        // Change code above this line
+    };
+    console.log(pizzaPalace.order('Smoked'));
+    console.log(pizzaPalace.order('Four meats'));
+    console.log(pizzaPalace.order('Big Mike'));
+    console.log(pizzaPalace.order('Viennese'));
+}
+//  === task 2
+{
+    const customer = {
+        username: 'Mango',
+        balance: 24000,
+        discount: 0.1,
+        orders: ['Burger', 'Pizza', 'Salad'],
+        // Change code below this line
+        getBalance() {
+            return this.balance;
+        },
+        getDiscount() {
+            return this.discount;
+        },
+        setDiscount(value) {
+            this.discount = value;
+        },
+        getOrders() {
+            return this.orders;
+        },
+        addOrder(cost, order) {
+            this.balance -= cost - cost * this.discount;
+            this.orders.push(order);
+        },
+        // Change code above this line
+    };
+
+    customer.setDiscount(0.15);
+    console.log(customer.getDiscount()); // 0.15
+    customer.addOrder(5000, 'Steak');
+    console.log(customer.getBalance()); // 19750
+    console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+}
+//  === task 3
+{
+}
+//  === task 4
+{
+}
+//  === task 5
+{
+}
+//  === task 6
+{
+}
+//  === task 7
+{
+}
+//  === task 8
+{
+}
+//  === task 9
+{
+}
+//  === task 10
+{
+}
+//  === task 11
+{
+}
+//  === task 12
+{
+}
+//  === task 13
+{
+}
+//  === task 14
+{
+}
+//  === task 15
+{
+}
+//  === task 16
+{
+}
+//  === task 17
+{
+}
+//  === task 18
+{
+}
+//  === task 19
+{
+}
+//  === task 20
+{
+}
